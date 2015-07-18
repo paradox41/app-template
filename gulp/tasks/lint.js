3d98ec74972f77029b4ce0f89b0345f0baeebff0
Will Soto <will.soto9@gmail.com>
2015-07-18 19:01:34 -0400

    chore(jscs): Remove jscs
    Eslint pretty much does it all

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,12 +3,10 @@ import config from '../config';
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import eslint from 'gulp-eslint';
-import jscs from 'gulp-jscs';
 
 gulp.task('lint', function() {
     return gulp.src(config.js.files)
         .pipe(plumber())
         .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(jscs());
+        .pipe(eslint.format());
 });
