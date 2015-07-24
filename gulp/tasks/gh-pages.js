9dbea5f72c00803d85ed82a7f7cb820b6fb1a7cb
Will Soto <will.soto9@gmail.com>
2015-07-24 14:04:36 -0400

    chore(gulp): Use config

--- gulp/tasks/gh-pages.js
+++ gulp/tasks/gh-pages.js
@@ -4,6 +4,6 @@ import gulp from 'gulp';
 import pages from 'gulp-gh-pages';
 
 gulp.task('gh-pages', function() {
-    return gulp.src('./docs/**/*')
+    return gulp.src(`${config.docs}/**/*`)
         .pipe(pages());
 });
