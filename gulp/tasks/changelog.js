916b84e44a5d2be275b108b0de91b512bf2bf4cd
Will Soto <will.soto9@gmail.com>
2015-07-27 17:07:04 -0400

    chore(gulp): Update changelog task

--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -12,9 +12,12 @@ gulp.task('changelog', function(done) {
         fs.writeFile('CHANGELOG.md', log, done);
     }
     fs.readFile('./package.json', 'utf8', function(err, data) {
-        var ref$ = JSON.parse(data);
-        var repository = ref$.repository;
-        var version = ref$.version;
+        if (err) {
+            return done(err);
+        }
+
+        let ref = JSON.parse(data);
+        let {repository, version} = ref;
 
         changelog({
             repository: repository.url,
