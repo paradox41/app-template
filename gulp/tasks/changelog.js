d5f70fa478040baea154fa719a63d39bc259dd83
Will Soto <will.soto9@gmail.com>
2015-08-01 14:38:04 -0400

    refactor(changelog): Update changelog task
    Use the official gulp version instead of the
    homerolled version

--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -1,27 +1,13 @@
-import fs from 'fs';
-
 import gulp from 'gulp';
 
-import changelog from 'conventional-changelog';
+import changelog from 'gulp-conventional-changelog';
 
-gulp.task('changelog', function(done) {
-    function changeParsed(err, log) {
-        if (err) {
-            return done(err);
-        }
-        fs.writeFile('CHANGELOG.md', log, done);
-    }
-    fs.readFile('./package.json', 'utf8', function(err, data) {
-        if (err) {
-            return done(err);
-        }
-
-        let ref = JSON.parse(data);
-        let {repository, version} = ref;
-
-        changelog({
-            repository: repository.url,
-            version: version
-        }, changeParsed);
-    });
+gulp.task('changelog', function() {
+    return gulp.src('CHANGELOG.md', {
+            buffer: false
+        })
+        .pipe(changelog({
+            preset: 'angular'
+        }))
+        .pipe(gulp.dest('./'));
 });
