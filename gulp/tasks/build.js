bcc1b2c32a348be516465b6b5eb62c15de9ec04a
Will Soto <will.soto9@gmail.com>
2015-08-16 14:46:45 -0400

    fix: Fix up release process

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,7 +3,6 @@ import runSequence from 'run-sequence';
 
 gulp.task('build', function() {
     runSequence('test:once', 'lint', 'clean', 'copy:build', [
-        // these are done async
         'browserify:build',
         'browserify:vendor:build',
         'scss:build'
--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -4,25 +4,72 @@ import git from 'gulp-git';
 import filter from 'gulp-filter';
 import tag from 'gulp-tag-version';
 
-var release = function(importance) {
-    return gulp.src(['./bower.json', './package.json'])
-        .pipe(bump({
-            type: importance
-        }))
-        .pipe(gulp.dest('./'))
-        .pipe(git.commit('chore(release): Bumps package version'))
-        .pipe(filter('bower.json'))
-        .pipe(tag());
+import runSequence from 'run-sequence';
+
+var config = {
+    importance: 'patch'
 };
 
-gulp.task('patch', ['build'], function() {
-    return release('patch');
+function getImportance() {
+    return config.importance;
+}
+
+function release() {
+    runSequence(
+        'test:once',
+        'lint',
+        'clean',
+        'copy:build', [
+            'browserify:build',
+            'browserify:vendor:build',
+            'scss:build'
+        ],
+        'cachebust',
+        'handlebars:build',
+        'bump',
+        'changelog',
+        'commit-release'
+    );
+}
+
+gulp.task('bump', function() {
+    return gulp.src([
+            './bower.json',
+            './package.json'
+        ])
+        .pipe(bump({
+            type: getImportance()
+        }))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('minor', ['build'], function() {
-    return release('minor');
+gulp.task('commit-release', function() {
+    return gulp.src([
+            './bower.json',
+            './package.json',
+            './CHANGELOG.md',
+            './dist'
+        ])
+        .pipe(git.add({
+            args: '-f -A'
+        }))
+        .pipe(git.commit('chore(release): New ' + getImportance() + ' release'))
+        .pipe(filter('bower.json'))
+        .pipe(tag());
 });
 
-gulp.task('major', ['build'], function() {
-    return release('major');
+gulp.task('release:patch', function() {
+    return release();
+});
+
+gulp.task('release:minor', function() {
+    config.importance = 'minor';
+
+    return release();
+});
+
+gulp.task('release:major', function() {
+    config.importance = 'major';
+
+    return release();
 });
