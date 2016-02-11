069e88807ccb4278cc2ff5be1e317179bc52ed9b
Will Soto <will.soto9@gmail.com>
2016-02-11 15:44:50 -0500

    style(eslint): lint .spec files now

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -2,9 +2,13 @@ import config from '../config';
 
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+import debug from 'gulp-debug';
 
 gulp.task('lint', function() {
     return gulp.src(config.js.files)
+        .pipe(debug({
+            title: '[eslint]'
+        }))
         .pipe(eslint())
         .pipe(eslint.format());
 });
