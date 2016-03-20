4c506b00ce7472dfa70b14ce374dcd9b0ffa1b67
Will Soto <will.soto9@gmail.com>
2016-03-20 12:55:54 -0400

    feat(stylelint): add stylelint

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -3,12 +3,28 @@ import config from '../config';
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import debug from 'gulp-debug';
+import stylelint from 'gulp-stylelint';
 
-gulp.task('lint', function() {
+gulp.task('lint', ['lint:scss'], function() {
     return gulp.src(config.js.files)
         .pipe(debug({
             title: '[eslint]'
         }))
         .pipe(eslint())
         .pipe(eslint.format());
+});
+
+gulp.task('lint:scss', function() {
+    return gulp.src(config.scss.files)
+        .pipe(debug({
+            title: '[stylelint]'
+        }))
+        .pipe(stylelint({
+            syntax: 'scss',
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }],
+            debug: true
+        }));
 });
