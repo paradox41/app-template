6bab7346f042f0f46d8237b60e35dc7be93ccc0b
Will Soto <will.soto9@gmail.com>
2016-04-26 09:07:18 +0100

    refactor(app): remove bootstrap and extra modules.
    Keep the project clean and simple.
    
    BREAKING CHANGES: no more bootstrap or font-awesome
    
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- app/index.js
+++ app/index.js
@@ -2,31 +2,15 @@ import angular from 'angular';
 
 import 'angular-ui-router';
 
-import resource from './common/resource';
-import filters from './common/filters';
-import navigation from './common/navigation';
-
-import users, {
-  userNavigation
-} from './users';
-
 import './app.scss';
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [
-  'ui.router',
-  users.name,
-  resource.name,
-  filters.name,
-  navigation.name
+  'ui.router'
 ])
 
-.config( /*@ngInject*/ function($stateProvider, $urlRouterProvider, ResourceConfigProvider) {
-
-  ResourceConfigProvider.setConfig({
-    baseURL: 'http://jsonplaceholder.typicode.com'
-  });
+.config( /*@ngInject*/ function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('');
 
@@ -37,13 +21,9 @@ angular.module(MODULE_NAME, [
   });
 })
 
-.constant('version', require('../package.json').version)
-
 .run( /*@ngInject*/ function($rootScope, $state, $stateParams) {
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
-
-  $rootScope.navItems = [userNavigation];
 
   $rootScope.$on('$routeChangeError', function() {
     console.log('failed to change routes', arguments);
--- app/users/models/user.js
+++
@@ -1,7 +0,0 @@
-import { extend } from 'lodash';
-
-export default class User {
-  constructor(user) {
-    extend(this, user);
-  }
-}
