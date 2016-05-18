e46acf24607141d2050ee7c6c15b3cdf173bbf32
Will Soto <will.soto9@gmail.com>
2016-05-18 19:45:59 -0400

    fix(server): reference the correct config
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 
-var config = require('./webpack.config');
+var config = require('./webpack.config').BASE_CONFIG;
 
 var compiler = webpack(config);
 
@@ -10,5 +10,6 @@ var server = new WebpackDevServer(compiler);
 var port = 8080;
 
 server.listen(port, 'localhost', function() {
+  /*eslint no-console:"off"*/
   console.log(`Listening on port ${port}`);
 });
