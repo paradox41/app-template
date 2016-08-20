a137892e6413dcadb5d44bbfa4005166dc35d6ff
Will Soto <will.soto9@gmail.com>
2016-08-20 13:17:38 -0400

    feat(webpack): add webpack-dashboard
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,1 +1,10 @@
-module.exports = require('./webpack.common');
+var webpackMerge = require('webpack-merge');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+
+var config = require('./webpack.common');
+
+module.exports = webpackMerge(config, {
+  plugins: [
+    new DashboardPlugin()
+  ]
+});
